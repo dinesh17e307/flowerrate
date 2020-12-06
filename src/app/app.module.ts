59e2f681141beffa7224b3f6ad9108f63dfef6dd
dinesh17e307 <dinisellappan@gmail.com>
2020-12-06 11:27:01 +0530

    5 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { MatListModule } from '@angular/material/list';
+import{BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import{MatToolbarModule} from '@angular/material/toolbar';
+
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { FootComponent } from './foot.component';
+import { HeadComponent } from './head.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent,FootComponent ],
+  imports:      [ BrowserModule, FormsModule,BrowserAnimationsModule,MatListModule
+  ,MatButtonModule,MatToolbarModule ],
+  declarations: [ AppComponent, HelloComponent,FootComponent,HeadComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/head.component.ts
@@ -0,0 +1,10 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-head',
+  templateUrl: './head.component.html',
+  styleUrls: [ './head.component.css' ]
+})
+export class HeadComponent  {
+  name = 'Angular ' + VERSION.major;
+}
