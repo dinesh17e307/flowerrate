5ff94045eb4584f185b1ceaf13c027c720188daa
dinesh17e307 <dinisellappan@gmail.com>
2020-12-08 18:23:11 +0530

    3 files modified, 7 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './appRouting.module';
 
 import { MatListModule } from '@angular/material/list';
 import{BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -13,14 +12,12 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { FootComponent } from './foot.component';
 import { HeadComponent } from './head.component';
-import { VedioComponent } from './vedio.component';
-import { HeaderComponent } from './header.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,BrowserAnimationsModule,MatListModule
-  ,MatButtonModule,MatToolbarModule ,YouTubePlayerModule,AppRoutingModule],
+  ,MatButtonModule,MatToolbarModule ,YouTubePlayerModule],
   declarations: [ AppComponent, HelloComponent,FootComponent,HeadComponent ,
-  VedioComponent,HeaderComponent],
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
